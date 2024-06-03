358e023d17a4592e2d79eb937a93a902c99a0eda
sakakky6825 <50253821+SkBoyZ@users.noreply.github.com>
2024-06-04 01:41:01 +0900

    add: post path user create

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,35 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { prismaClient } from "../utils/prisma";
 
 const userRoutes = new Elysia().group("/users", (app) => {
-  return app.get("/", async () => {
+  app.get("/", async () => {
     const users = await prismaClient.user.findMany();
 
     return { users };
   });
+
+  app.post(
+    "/",
+    async (req) => {
+      const body = req.body;
+
+      const user = await prismaClient.user.create({
+        data: body,
+      });
+
+      return { user };
+    },
+    {
+      body: t.Object({
+        username: t.String(),
+        password: t.String({
+          minLength: 8,
+        }),
+      }),
+    }
+  );
+
+  return app;
 });
 
 export default userRoutes;
