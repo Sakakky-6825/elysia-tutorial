47535afe61df3c3cef52d059d7b5ed5345e3791d
sakakky6825 <50253821+SkBoyZ@users.noreply.github.com>
2024-06-04 02:25:36 +0900

    fix: group method to construct prefix

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,40 +2,32 @@ import Elysia, { t } from "elysia";
 import { prismaClient } from "../utils/prisma";
 import { UserModel } from "../models/user";
 
-const userRoutes = new Elysia().group("/users", (app) => {
-  app
-    .use(UserModel)
-    .get("/", async () => {
-      return await prismaClient.user.findMany();
-    })
-    .get("/:id", async (req) => {
-      const { id } = req.params;
+const userRoutes = new Elysia({ prefix: "/users" })
+  .use(UserModel)
+  .get("/", async () => {
+    return await prismaClient.user.findMany();
+  })
+  .get("/:id", async (req) => {
+    const { id } = req.params;
 
-      return await prismaClient.user.findUnique({
-        where: {
-          id: Number(id),
-        },
-      });
-    })
-    .post(
-      "/",
-      async (req) => {
-        const body = req.body;
-
-        return await prismaClient.user.create({
-          data: {
-            username: body.username,
-            password: body.password,
-          },
-        });
+    return await prismaClient.user.findUnique({
+      where: {
+        id: Number(id),
       },
-      {
-        body: "user",
-        response: "user",
-      }
-    );
+    });
+  })
+  .post(
+    "/new",
+    async (req) => {
+      const body = req.body;
 
-  return app;
-});
+      return await prismaClient.user.create({
+        data: body,
+      });
+    },
+    {
+      body: "user",
+    }
+  );
 
 export default userRoutes;
